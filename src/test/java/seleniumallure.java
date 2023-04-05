6ceb14452cb5f85b84357e5887e5a560fabf4110
hasanefeavc <hasanefeavc@gmail.com>
2023-04-05 10:25:04 +0300

    init commit

--- src/test/java/seleniumallure.java
+++ src/test/java/seleniumallure.java
@@ -55,5 +55,26 @@ public class seleniumallure {
 
     }
 
+    @Test
+    public void googlesearch2() throws Exception {
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+        driver.get("https://www.google.com");
+
+        WebElement searchtext = driver.findElement(By.xpath("//input[@class='gLFyf']"));
+        searchtext.sendKeys("google");
+
+        searchtext.submit();
+
+        driver.quit();
+
+    }
+
 
 }
